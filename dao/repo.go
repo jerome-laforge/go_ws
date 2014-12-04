d0ab2f65329c9e8e0fc37e9431f884cdc74140f3
Jérôme LAFORGE <jerome.laforge@gmail.com>
2014-12-04 14:53:43 +0100

    Initial import

---
+++ dao/repo.go
@@ -0,0 +1,97 @@
+package dao
+
+import "database/sql"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jerome-laforge/go_ws/dto"
+	"time"
+)
+
+const (
+	addr   = "127.0.0.1:3306"
+	login  = "todos_rw"
+	passwd = "todos_rw"
+	db     = "todos"
+)
+const dbUrl = login + ":" + passwd + "@tcp(" + addr + ")/" + db + "?parseTime=true"
+
+func RepoGetTodos() dto.Todos {
+	con, err := sql.Open("mysql", dbUrl)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer con.Close()
+
+	rows, err := con.Query("select id, name, completed, due from todos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	todos := make(dto.Todos, 0, 10)
+	var curTodo dto.Todo
+	for rows.Next() {
+		err = rows.Scan(&curTodo.Id, &curTodo.Name, &curTodo.Completed, &curTodo.Due)
+		if err != nil {
+			panic(err)
+		}
+		todos = append(todos, curTodo)
+	}
+	return todos
+}
+
+func RepoCreateTodo(t dto.Todo) dto.Todo {
+	con, err := sql.Open("mysql", dbUrl)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer con.Close()
+
+	if t.Due.IsZero() {
+		t.Due = time.Now().Add(7 * 24 * time.Hour)
+	}
+
+	res, err := con.Exec("insert into todos (name, completed, due) values (?, ?, ?)", t.Name, t.Completed, t.Due)
+	if err != nil {
+		panic(err)
+	}
+
+	id, _ := res.LastInsertId()
+
+	t.Id = int(id)
+
+	return t
+}
+
+func RepoDestroyTodo(id int) (dto.Todo, error) {
+	con, err := sql.Open("mysql", dbUrl)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer con.Close()
+
+	if todo, ok:= RepoFindTodo(id); ok {
+		_, err = con.Exec("delete from todos where id = ?", id)
+		if err != nil {
+			return todo, err
+		}
+	}
+	return dto.Todo{}, nil
+}
+
+func RepoFindTodo(id int) (dto.Todo, bool) {
+	con, err := sql.Open("mysql", dbUrl)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer con.Close()
+
+	todo := new(dto.Todo)
+	row := con.QueryRow("select id, name, completed, due from todos where id = ?", id)
+	err = row.Scan(&todo.Id, &todo.Name, &todo.Completed, &todo.Due)
+	if err != nil {
+		return *todo, false
+	}
+
+	return *todo, true
+}
+
---
+++ handlers.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	//dao "github.com/jerome-laforge/go_ws/test/dao"
+	dao "github.com/jerome-laforge/go_ws/dao"
+	"github.com/jerome-laforge/go_ws/dto"
+	"github.com/gorilla/mux"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(dao.RepoGetTodos()); err != nil {
+		panic(err)
+	}
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var todoId int
+	var err error
+	if todoId, err = strconv.Atoi(vars["todoId"]); err != nil {
+		panic(err)
+	}
+
+	if todo, ok := dao.RepoFindTodo(todoId); ok {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(todo); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	// If we didn't find it, 404
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusNotFound)
+	if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
+		panic(err)
+	}
+
+}
+
+/*
+Test with this curl command:
+
+curl -H "Content-Type: application/json" -d '{"name":"New Todo"}' http://localhost:8080/todos
+
+*/
+func TodoCreate(w http.ResponseWriter, r *http.Request) {
+	var todo dto.Todo
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &todo); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	t := dao.RepoCreateTodo(todo)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
+}
+
+/*
+Test with this curl command:
+
+curl  -H "Content-Type: application/json"  -X DELETE http://localhost:8080/todos/2
+
+*/
+
+func TodoDelete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	if vars != nil {
+		if sTodoId, ok := vars["todoId"]; ok {
+			var todo dto.Todo
+			todoId, err := strconv.Atoi(sTodoId)
+			if err == nil {
+				todo, err = dao.RepoDestroyTodo(todoId)
+			}
+			if err != nil {
+				panic(err)
+			} else {
+				if err = json.NewEncoder(w).Encode(todo); err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
